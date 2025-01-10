8a4321242cb813acec1efcb381964553dc511ae6
Gayatri <gayatriloungani2021@gmail.com>
2025-01-11 02:01:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-      origin: "https://localhost:5173.com",
+      origin: "https://myownstore-frontend.onrender.com",
       methods: ["GET", "POST", "DELETE", "PUT"],
       allowedHeaders: [
         "Content-Type",
